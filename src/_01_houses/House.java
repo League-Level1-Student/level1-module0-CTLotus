fde255677ed2641fbd731991d26e774dcf1b09dd
60222 <60222@0338S-S-R0B6ZHH.sdcs.local>
2019-05-28 14:25:11 -0700

    5/28/2019

---
+++ src/_01_houses/House.java
@@ -0,0 +1,36 @@
+package _01_houses;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class House {
+	public static void main(String[] args) {
+		
+		Robot z = new Robot ();
+		z.moveTo(70,525);
+		z.setAngle(90);
+		z.penDown();
+		z.move(30);
+		z.setAngle(0);
+		z.move(150);
+		z.setAngle(90);
+		z.move(80);
+		z.setAngle(180);
+		z.move(150);
+		z.setAngle(90);
+		z.move(30);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
